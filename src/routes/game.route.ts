09b037b4927b8bc99b92ce778179977b9dd99e37
mukesh-kickr <mukesh@kickrtechnology.in>
2026-02-28 16:24:10 +0530

    Routes updated for events, banners and match rules

--- src/routes/game.route.ts
+++ src/routes/game.route.ts
@@ -1,9 +1,21 @@
 import express from "express";
 import { createGame, getGames } from "../controllers/game.controller.js";
 import { upload } from "../middleware/multer.js";
+import { createBanner, getBanners } from "../controllers/banner.controller.js";
+import { createEvent, getEvents } from "../controllers/event.controller.js";
+import { createRule, getRules } from "../controllers/rule.controller.js";
 
 const router = express.Router();
 router.post("/",upload.single("gameImage"), createGame)
 router.get("/", getGames)
+
+router.post("/banners", upload.single("image"), createBanner)
+router.get("/banners", getBanners)
+
+router.post("/events", createEvent)
+router.get("/events", getEvents)
+
+router.post("/rules", createRule)
+router.get("/rules", getRules)
 
 export default router
